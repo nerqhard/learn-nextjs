00ff0e941e0aa175972d9d64b0deeae27026d9fe
Quang Ha <quangha12thxd@gmail.com>
2022-04-30 23:15:18 +0700

    Add video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [isMount, setIsMount] = useState(false)
+
+  useEffect(() => {
+    setIsMount(true)
+
+    return () => {
+      setIsMount(false)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,50 +26,25 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Pé JI 610 🫶</a>
+          Welcome to <a href="https://www.youtube.com/watch?v=Qy_ADdVXv6Q&ab_channel=1967">Pé JI 610 🫶</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+        <div className={styles.description}>
+          {isMount && (
+            <ReactPlayer
+              className="react-player fixed-bottom"
+              // url= 'https://www.youtube.com/watch?v=FGz4DAylafk&ab_channel=XPEDM'
+              url="videos/test.mp4"
+              width="100%"
+              height="100%"
+              controls
+            />
+          )}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Powered by <span className={styles.logo}>🫶🫶🫶</span>
       </footer>
     </div>
   )
